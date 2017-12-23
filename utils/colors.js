0e6804cd7fc45af7b8d2f068dff45e8774431ce8
Ahmad Yousef <ahmad@alukah.net>
2017-12-23 15:51:26 +0300

    Colors ,,
    Somehow, this file didn’t upload

---
+++ utils/colors.js
@@ -0,0 +1,12 @@
+// See https://www.rapidtables.com/web/color/
+
+export const purple = '#292477'
+export const darkPurple = '#1e1a5e'
+export const gray = '#757575'
+export const white = '#fff'
+export const red = '#b71845'
+export const orange = '#f26f28'
+export const blue = '#4e4cb8'
+export const lightPurp = '#7c53c3'
+export const pink = '#b93fb3'
+export const green = '#008000'
